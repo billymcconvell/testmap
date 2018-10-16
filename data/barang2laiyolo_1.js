a6d52fc173f1288d987dcf50f61f77e3d0427570
billymcconvell <43562809+billymcconvell@users.noreply.github.com>
2018-10-16 15:00:41 +0930

    Add files via upload

---
+++ data/barang2laiyolo_1.js
@@ -0,0 +1,9 @@
+var json_barang2laiyolo_1 = {
+"type": "FeatureCollection",
+"name": "barang2laiyolo_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "name": "Barang-barang" }, "geometry": { "type": "Point", "coordinates": [ 120.469943323138367, -6.405896951160641 ] } },
+{ "type": "Feature", "properties": { "id": null, "name": "Laiyolo" }, "geometry": { "type": "Point", "coordinates": [ 120.482522976050149, -6.283830062135376 ] } }
+]
+}
